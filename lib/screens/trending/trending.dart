aeecededd6ca209e35c115236aff629334c92afb
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-09-01 21:00:15 +0800

    refactor: remove unused imports from TrendingScreen

--- lib/screens/trending/trending.dart
+++ lib/screens/trending/trending.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_aigun/cubits/index.dart';
-import 'package:flutter_aigun/utils/resource.dart';
-import 'package:flutter_aigun/widgets/smart_network_image.dart';
 import 'package:provider/provider.dart';
 
 class TrendingScreen extends StatefulWidget {
