40935178da4e1b2b58a5fd48fd7cd196d2ea1670
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-09-05 23:48:09 +0800

    feat: enhance token selector sheet by improving search input design and functionality, including clipboard paste support

--- lib/utils/sheet/token_selector_sheet.dart
+++ lib/utils/sheet/token_selector_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aigun/utils/clipboard.dart';
 import 'package:flutter_aigun/widgets/search.dart';
 import 'package:flutter_aigun/widgets/token/models/token.dart';
 import 'package:flutter_aigun/themes/index.dart';
@@ -17,6 +18,7 @@ Future<Token?> showTokenSelectorSheet(
   final result = await showModalBottomSheet<Token?>(
       context: context,
       isScrollControlled: true,
+      backgroundColor: AppColors.background(context),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
       ),
@@ -62,14 +64,7 @@ Future<Token?> showTokenSelectorSheet(
                   ),
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 16.w),
-                    child: SearchInput(
-                        hintText: "搜索名称或合约地址",
-                        prefixIcon: Icon(Icons.search,
-                            size: 20.w,
-                            color: AppColors.textSecondary(context)),
-                        backgroundColor: AppColors.card(context),
-                        controller: controller,
-                        borderRadius: BorderRadius.circular(10.r)),
+                    child: _buildSearchInput(context),
                   ),
                   Expanded(
                       child: TokenList(
@@ -84,69 +79,54 @@ Future<Token?> showTokenSelectorSheet(
   return result;
 }
 
-// Widget _buildSearchInput(BuildContext context) {
-//   final TextEditingController searchController = TextEditingController();
-//   return SizedBox(
-//     height: 40,
-//     child: TextField(
-//       controller: searchController,
-//       decoration: InputDecoration(
-//         contentPadding: EdgeInsets.zero, // 去掉内边距 才能让文本居中
-//         hintText: "搜索名称或合约地址",
-//         hintStyle: const TextStyle(color: Colors.grey),
-//         // prefixIcon: const Icon(Icons.search_sharp),
-//         prefixIcon: Padding(
-//           padding: const EdgeInsets.symmetric(horizontal: 14.0),
-//           child: SvgPicture.asset(
-//             "assets/images/icons/lightning-search.svg",
-//             width: 16,
-//             height: 16,
-//             colorFilter: ColorFilter.mode(
-//               AppColors.textSecondary(context),
-//               BlendMode.srcIn,
-//             ),
-//           ),
-//         ),
-//         suffixIcon: TextButton(
-//           onPressed: () {
-//             ClipboardUtils.paste().then((value) {
-//               print("Paste: $value");
-//               searchController.text = value;
-//             });
-//           },
-//           child: Container(
-//             padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-//             decoration: BoxDecoration(
-//               color: Colors.red[500]!.withValues(alpha: 0.5),
-//               borderRadius: BorderRadius.circular(10),
-//             ),
-//             child: const Row(
-//               mainAxisSize: MainAxisSize.min,
-//               mainAxisAlignment: MainAxisAlignment.end,
-//               children: [
-//                 Icon(
-//                   Icons.copy_all_outlined,
-//                   color: Colors.white,
-//                 ),
-//                 Text("Paste",
-//                     style: TextStyle(color: Colors.white, fontSize: 14)),
-//               ],
-//             ),
-//           ),
-//         ),
-//         border: OutlineInputBorder(
-//             borderSide:
-//                 BorderSide(color: AppColors.textQuinary(context), width: 1),
-//             borderRadius: const BorderRadius.all(Radius.circular(20))),
-//         enabledBorder: OutlineInputBorder(
-//             borderSide:
-//                 BorderSide(color: AppColors.textQuinary(context), width: 1),
-//             borderRadius: const BorderRadius.all(Radius.circular(20))),
-//         focusedBorder: OutlineInputBorder(
-//             borderSide:
-//                 BorderSide(color: AppColors.textQuinary(context), width: 1),
-//             borderRadius: const BorderRadius.all(Radius.circular(20))),
-//       ),
-//     ),
-//   );
-// }
+Widget _buildSearchInput(BuildContext context) {
+  final TextEditingController searchController = TextEditingController();
+  return SizedBox(
+    height: 46.h,
+    child: TextField(
+      controller: searchController,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: AppColors.card(context),
+        contentPadding: EdgeInsets.zero, // 去掉内边距 才能让文本居中
+        hintText: "搜索名称或合约地址",
+        hintStyle: TextStyle(color: AppColors.textSecondary(context)),
+        // prefixIcon: const Icon(Icons.search_sharp),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+          child: Icon(Icons.search,
+              size: 25.w, color: AppColors.textSecondary(context)),
+        ),
+        suffixIcon: Container(
+          margin: EdgeInsets.all(6.w),
+          padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+          decoration: BoxDecoration(
+            color: AppColors.quaternary,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(
+                Icons.copy_all_outlined,
+                color: Colors.white,
+              ),
+              Text("Paste",
+                  style: TextStyle(color: Colors.white, fontSize: 14)),
+            ],
+          ),
+        ),
+        border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+      ),
+    ),
+  );
+}
