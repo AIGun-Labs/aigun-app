f51b2b63b6af37ad3c4ade8c23c0230c05822123
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-09-15 17:36:42 +0800

    feat: implement custom tab bar with animated icons using Lottie and SVG assets, enhancing user interaction and visual appeal

--- lib/screens/tabbar/tabbar.dart
+++ lib/screens/tabbar/tabbar.dart
@@ -1,3 +1,4 @@
+import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_aigun/l10n/l10n.dart';
 import 'package:flutter_aigun/screens/intel/intel.dart';
@@ -9,6 +10,7 @@ import 'package:flutter_aigun/themes/themes.dart';
 import 'package:flutter_aigun/widgets/keep_alive_page.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
 class TabbarScreen extends StatefulWidget {
   const TabbarScreen({super.key});
@@ -110,29 +112,116 @@ class TabbarScreenState extends State<TabbarScreen> {
     return items;
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //
+  // return Scaffold(
+  //     key: scaffoldKey,
+  //     bottomNavigationBar: Container(
+  //       decoration: BoxDecoration(
+  //         border: Border(
+  //           top: BorderSide(
+  //             color: AppColors.borderSecondary(context), // 使用应用主题的边框颜色
+  //             width: 1.0,
+  //           ),
+  //         ),
+  //       ),
+  //       child: BottomNavigationBar(
+  //         items: _buildBottomNavigationBarItems(context),
+  //         currentIndex: _selectedIndex,
+  //         onTap: _updateSelectedIndex,
+  //       ),
+  //     ),
+  //     body: IndexedStack(
+  //       index: _selectedIndex,
+  //       children: _pages,
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            top: BorderSide(
-              color: AppColors.borderSecondary(context), // 使用应用主题的边框颜色
-              width: 1.0,
-            ),
-          ),
-        ),
-        child: BottomNavigationBar(
-          items: _buildBottomNavigationBarItems(context),
-          currentIndex: _selectedIndex,
-          onTap: _updateSelectedIndex,
-        ),
-      ),
       body: IndexedStack(
         index: _selectedIndex,
         children: _pages,
       ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            border: Border(
+                top: BorderSide(
+                    color: AppColors.borderSecondary(context), width: 1.0))),
+        child: BottomNavigationBar(items: [
+          BottomNavigationBarItem(
+            label: "Intel",
+            icon: TabItem(
+                svgPath: "assets/tabbar/intel.svg",
+                lottiePath: "assets/lottie/cap.lottie",
+                unselectedColor: AppColors.textPrimary(context),
+                isSelected: true),
+          ),
+          BottomNavigationBarItem(
+            label: "Trending",
+            icon: TabItem(
+                svgPath: "assets/tabbar/trending.svg",
+                lottiePath: "assets/lottie/cap.lottie",
+                unselectedColor: AppColors.textPrimary(context),
+                isSelected: false),
+          ),
+        ]),
+      ),
     );
+  }
+}
+
+class TabItem extends StatelessWidget {
+  const TabItem(
+      {super.key,
+      required this.svgPath,
+      required this.lottiePath,
+      required this.unselectedColor,
+      required this.isSelected});
+  final String svgPath;
+  final String lottiePath;
+  final Color unselectedColor;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(microseconds: 300),
+      transitionBuilder: (child, animation) => FadeTransition(
+        opacity: animation,
+        child: ScaleTransition(
+          scale: animation,
+          child: child,
+        ),
+      ),
+      child: isSelected
+          ? TabActiveIcon(path: lottiePath)
+          : SvgPicture.asset(
+              svgPath,
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(unselectedColor, BlendMode.srcATop),
+            ),
+    );
+  }
+}
+
+class TabActiveIcon extends StatelessWidget {
+  const TabActiveIcon({super.key, required this.path});
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return DotLottieLoader.fromAsset(path, frameBuilder: (context, dotlottie) {
+      if (dotlottie != null) {
+        return Lottie.memory(dotlottie.animations.values.single,
+            width: 24, height: 24);
+      }
+      return const SizedBox.shrink();
+    });
   }
 }
