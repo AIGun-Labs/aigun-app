b9ed61b49be03bf88ae4025337c95195d5062ab1
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-09-29 18:08:37 +0800

    feat: Enhance environment configuration with obfuscation, add AI agent model and API integration, and implement loading functionality in trending screen

---
+++ lib/data/models/trending/ai_agent/ai_agent.dart
@@ -0,0 +1,35 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../language/language.dart';
+
+part 'ai_agent.freezed.dart';
+part 'ai_agent.g.dart';
+
+@freezed
+class AiAgent with _$AiAgent {
+  const factory AiAgent({
+    @Default('') String id,
+    @Default(Language()) Language name,
+    @Default(Language()) Language description,
+    @Default('') String avatar,
+    @Default(0) int rank,
+    @JsonKey(
+      name: 'is_followed',
+    )
+    @Default(false)
+    bool isFollowed,
+    @JsonKey(
+      name: 'subset_id',
+    )
+    @Default('')
+    String subsetId,
+    @JsonKey(
+      name: 'tag_id',
+    )
+    @Default('')
+    String tagId,
+  }) = _AiAgent;
+
+  factory AiAgent.fromJson(Map<String, dynamic> json) =>
+      _$AiAgentFromJson(json);
+}
