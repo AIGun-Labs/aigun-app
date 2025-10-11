04917ec53243d0ec941cb823e9e3a1b1f38e5ae7
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-10-11 19:15:07 +0800

    refactor: improve Android build workflow with keystore validation and environment configuration
    - Enhanced the Android build workflow by adding validation checks for the keystore file creation, ensuring it is not empty and exists after decoding.
    - Refactored environment variable handling in the EnvConfig class to utilize abstract classes for better structure and maintainability.
    - Removed the obsolete image.png file from the repository.

--- lib/config/env/env.dart
+++ lib/config/env/env.dart
@@ -31,80 +31,10 @@ enum EnvType {
 /// - .env.development: 默认开发环境配置
 /// - .env.development1.local: 本地开发环境1配置
 /// - .env.development2.local: 本地开发环境2配置
-
-// 定义每个环境的抽象类
-@Envied(path: ".env.production", obfuscate: true)
-abstract class _ProdEnv {
-  @EnviedField(varName: "BASE_API_URL")
-  static String? baseApiUrl;
-
-  @EnviedField(varName: "SENTRY_DSN")
-  static String? sentryDsn;
-
-  @EnviedField(varName: "BASE_WS_URL")
-  static String? wsUrl;
-
-  @EnviedField(varName: "PRIVATE_KEY")
-  static String? privateKey;
-
-  @EnviedField(varName: "BASE_CDN_URL")
-  static String? cdn;
-}
-
-@Envied(path: ".env.development", obfuscate: true)
-abstract class _DebugEnv {
-  @EnviedField(varName: "BASE_API_URL")
-  static String? baseApiUrl;
-
-  @EnviedField(varName: "SENTRY_DSN")
-  static String? sentryDsn;
-
-  @EnviedField(varName: "BASE_WS_URL")
-  static String? wsUrl;
-
-  @EnviedField(varName: "PRIVATE_KEY")
-  static String? privateKey;
-
-  @EnviedField(varName: "BASE_CDN_URL")
-  static String? cdn;
-}
-
-@Envied(path: ".env.development1.local", obfuscate: true)
-abstract class _Dev1Env {
-  @EnviedField(varName: "BASE_API_URL")
-  static String? baseApiUrl;
-
-  @EnviedField(varName: "SENTRY_DSN")
-  static String? sentryDsn;
-
-  @EnviedField(varName: "BASE_WS_URL")
-  static String? wsUrl;
-
-  @EnviedField(varName: "PRIVATE_KEY")
-  static String? privateKey;
-
-  @EnviedField(varName: "BASE_CDN_URL")
-  static String? cdn;
-}
-
-@Envied(path: ".env.development2.local", obfuscate: true)
-abstract class _Dev2Env {
-  @EnviedField(varName: "BASE_API_URL")
-  static String? baseApiUrl;
-
-  @EnviedField(varName: "SENTRY_DSN")
-  static String? sentryDsn;
-
-  @EnviedField(varName: "BASE_WS_URL")
-  static String? wsUrl;
-
-  @EnviedField(varName: "PRIVATE_KEY")
-  static String? privateKey;
-
-  @EnviedField(varName: "BASE_CDN_URL")
-  static String? cdn;
-}
-
+@Envied(path: ".env.production", name: "ProdEnv", obfuscate: true)
+@Envied(path: ".env.development", name: "DebugEnv", obfuscate: true)
+@Envied(path: ".env.development1.local", name: "Dev1Env", obfuscate: true)
+@Envied(path: ".env.development2.local", name: "Dev2Env", obfuscate: true)
 final class EnvConfig {
   static const bool kDebugMode = foundation.kDebugMode;
 
@@ -127,42 +57,25 @@ final class EnvConfig {
 
   factory EnvConfig() => _instance;
 
-  static final EnvConfig _instance = EnvConfig._internal();
+  static final EnvConfig _instance = switch (currentEnvType) {
+    EnvType.production => _ProdEnv(),
+    EnvType.development1 => _Dev1Env(),
+    EnvType.development2 => _Dev2Env(),
+    EnvType.development => _DebugEnv(),
+  };
 
-  EnvConfig._internal();
+  @EnviedField(varName: "BASE_API_URL")
+  final String baseApiUrl = _instance.baseApiUrl;
 
-  String get baseApiUrl => switch (currentEnvType) {
-        EnvType.production => _ProdEnv.baseApiUrl!,
-        EnvType.development1 => _Dev1Env.baseApiUrl!,
-        EnvType.development2 => _Dev2Env.baseApiUrl!,
-        EnvType.development => _DebugEnv.baseApiUrl!,
-      };
+  @EnviedField(varName: "SENTRY_DSN")
+  final String sentryDsn = _instance.sentryDsn;
 
-  String get sentryDsn => switch (currentEnvType) {
-        EnvType.production => _ProdEnv.sentryDsn!,
-        EnvType.development1 => _Dev1Env.sentryDsn!,
-        EnvType.development2 => _Dev2Env.sentryDsn!,
-        EnvType.development => _DebugEnv.sentryDsn!,
-      };
+  @EnviedField(varName: "BASE_WS_URL")
+  final String wsUrl = _instance.wsUrl;
 
-  String get wsUrl => switch (currentEnvType) {
-        EnvType.production => _ProdEnv.wsUrl!,
-        EnvType.development1 => _Dev1Env.wsUrl!,
-        EnvType.development2 => _Dev2Env.wsUrl!,
-        EnvType.development => _DebugEnv.wsUrl!,
-      };
+  @EnviedField(varName: "PRIVATE_KEY")
+  final String privateKey = _instance.privateKey;
 
-  String get privateKey => switch (currentEnvType) {
-        EnvType.production => _ProdEnv.privateKey!,
-        EnvType.development1 => _Dev1Env.privateKey!,
-        EnvType.development2 => _Dev2Env.privateKey!,
-        EnvType.development => _DebugEnv.privateKey!,
-      };
-
-  String get cdn => switch (currentEnvType) {
-        EnvType.production => _ProdEnv.cdn!,
-        EnvType.development1 => _Dev1Env.cdn!,
-        EnvType.development2 => _Dev2Env.cdn!,
-        EnvType.development => _DebugEnv.cdn!,
-      };
+  @EnviedField(varName: "BASE_CDN_URL")
+  final String cdn = _instance.cdn;
 }
