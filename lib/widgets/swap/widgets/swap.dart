18a75acf26417e5c9c7fc852432bf311bbb1d6e3
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-11-11 23:28:27 +0800

    fix: update balance display in TradeSwap to use fromBalance instead of amount

--- lib/widgets/swap/widgets/swap.dart
+++ lib/widgets/swap/widgets/swap.dart
@@ -160,7 +160,7 @@ class _TradeSwapState extends State<TradeSwap> {
                   ),
                   Text(
                       CurrencyFormatter.abbreviateTokenPrice(
-                          double.tryParse(state.amount) ?? 0,
+                          state.fromBalance ?? 0,
                           fixedDecimals: 4),
                       style: TextStyle(
                           fontSize: 16.sp,
