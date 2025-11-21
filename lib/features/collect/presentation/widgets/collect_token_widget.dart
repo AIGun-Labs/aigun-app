034e38d6a542f94cc65ecd989d6b9daba7e28895
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-11-21 18:42:11 +0800

    style: unify page margins for wallet、bonus and trending pages

--- lib/features/collect/presentation/widgets/collect_token_widget.dart
+++ lib/features/collect/presentation/widgets/collect_token_widget.dart
@@ -115,7 +115,7 @@ class _CollectTokenWidgetState extends State<CollectTokenWidget> {
           child: ListTile(
             key: ValueKey('trending_item_${widget.index}'),
             contentPadding: EdgeInsets.symmetric(
-              horizontal: 20.w,
+              horizontal: 15.w,
             ),
             horizontalTitleGap: 12.w,
             leading: ClipOval(
