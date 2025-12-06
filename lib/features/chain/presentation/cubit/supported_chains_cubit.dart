ad80efd5add89f3b957373ea11c89918fa330993
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-12-06 05:43:54 +0200

    refactor(chain): remove deprecated chains cubit and state
    - Deleted ChainsCubit and ChainsState as part of the transition to SupportedChainsCubit and SupportedChainsStatus.
    - Introduced SupportedChainsCubit to manage the state of supported blockchain networks.
    - Added SupportedChainsStatus to handle various states of the supported chains feature.

---
+++ lib/features/chain/presentation/cubit/supported_chains_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/types/result.dart';
+import '../../application/usecases/get_supported_chains.dart';
+import 'supported_chains_state.dart';
+
+class SupportedChainsCubit extends Cubit<SupportedChainsStatus> {
+  final GetSupportedChains _getSupportedChains;
+  SupportedChainsCubit({required GetSupportedChains getSupportedChains})
+    : _getSupportedChains = getSupportedChains,
+      super(const SupportedChainsStatus.initial());
+
+  Future<void> getChains() async {
+    emit(const SupportedChainsStatus.loading());
+
+    final result = await _getSupportedChains.call();
+    result.whenOrNull(
+      success: (chains) => emit(SupportedChainsStatus.success(chains)),
+      failure: (failure) => emit(SupportedChainsStatus.error(failure)),
+    );
+  }
+}
