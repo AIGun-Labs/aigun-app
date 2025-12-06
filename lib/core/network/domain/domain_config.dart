fad5426531c64f82144be394ad7bde7b7522edef
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-12-06 15:12:06 +0800

    fix: reorder production domains in DomainConfig
    - Moved 'https://api.ak-47.dev' to the correct position in the prodDomains list within DomainConfig to ensure proper domain configuration.

--- lib/core/network/domain/domain_config.dart
+++ lib/core/network/domain/domain_config.dart
@@ -1,7 +1,7 @@
 class DomainConfig {
   static const List<String> prodDomains = [
-    'https://api.ak-47.dev',
     'https://api.route.aigun.ai',
+    'https://api.ak-47.dev',
   ];
 
   static const List<String> testDomains = ['https://t-api.route.aigun.ai'];
