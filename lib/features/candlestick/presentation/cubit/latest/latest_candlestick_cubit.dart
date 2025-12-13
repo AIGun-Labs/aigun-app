116258a8135f2b62a6ab2bc69721889130720eaf
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-12-13 16:28:59 +0800

    fix(latest_candlestick): ensure proper error handling and reset cancel token
    - Improved error handling in LatestCandlestickCubit by formatting the error message.
    - Reset the cancel token to null in clearData method to prevent potential memory leaks.

--- lib/features/candlestick/presentation/cubit/latest/latest_candlestick_cubit.dart
+++ lib/features/candlestick/presentation/cubit/latest/latest_candlestick_cubit.dart
@@ -109,7 +109,9 @@ class LatestCandlestickCubit extends Cubit<LatestCandlestickState> {
           state.copyWith(status: FetchLatestCandlestickStatus.error(error)),
         ),
         be: (reason) => emit(
-          state.copyWith(status: FetchLatestCandlestickStatus.error(reason.msg)),
+          state.copyWith(
+            status: FetchLatestCandlestickStatus.error(reason.msg),
+          ),
         ),
       );
     } finally {
@@ -126,5 +128,6 @@ class LatestCandlestickCubit extends Cubit<LatestCandlestickState> {
   void clearData() {
     emit(const LatestCandlestickState());
     _cancelToken?.cancel('clear data');
+    _cancelToken = null;
   }
 }
