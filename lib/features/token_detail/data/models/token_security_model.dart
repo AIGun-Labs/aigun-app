43a382daaf68b8846bd0297c0ebf4231ed1b67d5
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-12-15 16:28:57 +0800

    refactor(token_security_model): reorganize model properties and enhance JSON serialization
    - Moved the contractAnalysis and tradeTax properties back to the constructor for clarity.
    - Updated the SecurityItemModel and TradeTaxModel classes to maintain consistent structure and improve JSON serialization.
    - Removed redundant code to streamline the TokenSecurityModel class.

--- lib/features/token_detail/data/models/token_security_model.dart
+++ lib/features/token_detail/data/models/token_security_model.dart
@@ -8,6 +8,10 @@ part 'token_security_model.g.dart';
 @freezed
 @JsonSerializable()
 class TokenSecurityModel with _$TokenSecurityModel {
+  const TokenSecurityModel({required this.contractAnalysis, this.tradeTax});
+
+  factory TokenSecurityModel.fromJson(Map<String, dynamic> json) =>
+      _$TokenSecurityModelFromJson(json);
   @override
   @JsonKey(name: 'contract_analysis', defaultValue: <SecurityItemModel>[])
   final List<SecurityItemModel> contractAnalysis;
@@ -15,19 +19,20 @@ class TokenSecurityModel with _$TokenSecurityModel {
   @override
   @JsonKey(name: 'trade_tax')
   final TradeTaxModel? tradeTax;
-
-  const TokenSecurityModel({
-    this.contractAnalysis = const <SecurityItemModel>[],
-    this.tradeTax,
-  });
-
-  factory TokenSecurityModel.fromJson(Map<String, dynamic> json) =>
-      _$TokenSecurityModelFromJson(json);
 }
 
 // 普通 DTO + JsonSerializable
 @JsonSerializable()
 class SecurityItemModel {
+  SecurityItemModel({
+    this.title,
+    this.description,
+    this.isSafe = false,
+    this.type = 'risk',
+  });
+
+  factory SecurityItemModel.fromJson(Map<String, dynamic> json) =>
+      _$SecurityItemModelFromJson(json);
   @JsonKey(name: 'title')
   final Multilingual? title;
 
@@ -40,31 +45,20 @@ class SecurityItemModel {
   @JsonKey(name: 'type', defaultValue: 'risk')
   final String type;
 
-  SecurityItemModel({
-    this.title,
-    this.description,
-    this.isSafe = false,
-    this.type = 'risk',
-  });
-
-  factory SecurityItemModel.fromJson(Map<String, dynamic> json) =>
-      _$SecurityItemModelFromJson(json);
-
   Map<String, dynamic> toJson() => _$SecurityItemModelToJson(this);
 }
 
 @JsonSerializable()
 class TradeTaxModel {
+  const TradeTaxModel({required this.buyTax, required this.sellTax});
+
+  factory TradeTaxModel.fromJson(Map<String, dynamic> json) =>
+      _$TradeTaxModelFromJson(json);
   @JsonKey(name: 'buy_tax')
   final String buyTax;
 
   @JsonKey(name: 'sell_tax')
   final String sellTax;
-
-  const TradeTaxModel({required this.buyTax, required this.sellTax});
-
-  factory TradeTaxModel.fromJson(Map<String, dynamic> json) =>
-      _$TradeTaxModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$TradeTaxModelToJson(this);
 }
