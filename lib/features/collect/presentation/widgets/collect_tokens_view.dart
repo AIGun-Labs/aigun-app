cb3eaa0f1b692528019bfdad9699888cfb9ca926
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-12-17 22:33:46 +0800

    feat(collect): integrate sound effect on token collection action
    - Added a call to `SoundEffectCubit` to play a sound effect when a token is successfully collected, enhancing user feedback.
    - Updated toast messages to provide clearer notifications based on the collection status.

--- lib/features/collect/presentation/widgets/collect_tokens_view.dart
+++ lib/features/collect/presentation/widgets/collect_tokens_view.dart
@@ -5,6 +5,7 @@ import 'package:pull_to_refresh_notification/pull_to_refresh_notification.dart';
 
 import '../../../../core/router/routes/app_routes.dart';
 import '../../../../cubits/quick_trade/quick_trade_cubit.dart';
+import '../../../../cubits/sound_effect/sound_effect_cubit.dart';
 import '../../../../l10n/l10n.dart';
 import '../../../../shared/domain/entities/base_token_entity.dart';
 import '../../../../shared/domain/mappers/token_entity_mapper.dart';
@@ -55,6 +56,7 @@ class _CollectTokensViewState extends State<CollectTokensView>
     if (isCollected) {
       ToastUtils.showCenterToast(context, S.of(context).cancelTracking);
     } else {
+      await BlocProvider.of<SoundEffectCubit>(context).playGunLoad();
       ToastUtils.showCenterToast(context, S.of(context).trackSuccess);
     }
   }
