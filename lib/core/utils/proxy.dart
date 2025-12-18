53c7894a6f129ef44c15ebfc40873d799bf1a266
Kian Lian <93166482+kian-lian@users.noreply.github.com>
2025-12-18 21:11:07 +0800

    refactor(proxy): enhance ProxyUtils for URL handling
    - Renamed method `voide` to `video` for clarity and updated its implementation to utilize a new private method `_joinProxyUrl`.
    - Introduced `_joinProxyUrl` to centralize URL modification logic, allowing for better handling of proxy URLs and base URL checks.
    - Integrated `AppConfig` to dynamically fetch the base URL, improving flexibility and maintainability of URL modifications.

--- lib/core/utils/proxy.dart
+++ lib/core/utils/proxy.dart
@@ -1,5 +1,39 @@
+import '../../config/app_config.dart';
+
 class ProxyUtils {
-  static String voide(String url) {
-    return url.replaceAll('https://', 'https://proxy.aigun.io/');
+  static String video(String url) {
+    return _joinProxyUrl(url, useProxy: true);
+  }
+
+  static String _joinProxyUrl(String? path, {required bool useProxy}) {
+    if (path == null) {
+      return '';
+    }
+
+    final appConfig = AppConfig();
+    final baseUrl = appConfig.env.cdn;
+
+    if (path.startsWith(baseUrl)) {
+      return path;
+    }
+
+    // Remove trailing slash from baseUrl if present
+    final cleanBaseUrl = baseUrl.endsWith('/')
+        ? baseUrl.substring(0, baseUrl.length - 1)
+        : baseUrl;
+
+    // Remove leading slash from path if present
+    final cleanPath = path.startsWith('/') ? path.substring(1) : path;
+
+    if (!path.startsWith('http')) {
+      return '$cleanBaseUrl/$cleanPath';
+    }
+
+    if (useProxy) {
+      final url = '${appConfig.env.baseApiUrl}/api/v1/proxy?url=$cleanPath';
+      return url;
+    }
+
+    return path;
   }
 }
